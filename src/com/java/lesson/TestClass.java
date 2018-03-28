21767c8f8617258ca5b3f076ac37e80b97ff9293
killroy13 <hate66613@mail.ru>
2018-03-28 09:38:05 +0300

    create test class

---
+++ src/com/java/lesson/TestClass.java
@@ -0,0 +1,13 @@
+package com.java.lesson;
+
+/**
+ * Created by User on 28.03.2018.
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("tests file");
+        System.out.println("tests file");
+        System.out.println("tests file");
+        System.out.println("tests file");
+    }
+}
